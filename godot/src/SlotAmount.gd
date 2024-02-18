d136789c611f7453c97d811b8b5022d98eb38ec8
myin <minyin142p@gmail.com>
2024-02-18 18:55:26 +0100

    add SlotAmount script

---
+++ godot/src/SlotAmount.gd
@@ -0,0 +1,27 @@
+class_name SlotAmount
+extends TextureButton
+
+@export var slot_marker: SlotMarker
+@export var out_of_stock_color := Color.RED
+@export var full_stock_color := Color.GREEN
+@export var half_stock_color := Color.ORANGE
+
+func _ready():
+	pressed.connect(func(): GameManager.fill_slot(slot_marker.slot_coord))
+	GameManager.slot_filled.connect(func(slot, _x):
+		if slot_marker.slot_coord == slot:
+			_update_stock_color()
+	)
+	GameManager.slot_sold.connect(func(slot, _x):
+		if slot_marker.slot_coord == slot:
+			_update_stock_color()
+	)
+
+func _update_stock_color():
+	var p = GameManager.get_slot_amount(slot_marker.slot_coord) / float(GameManager.items_per_slot)
+	var color = half_stock_color
+	if p > 0.5:
+		color = full_stock_color
+	elif p == 0:
+		color = out_of_stock_color
+	modulate = color
--- godot/src/SlotMarker.gd
+++ godot/src/SlotMarker.gd
@@ -4,22 +4,15 @@ extends Marker2D
 const GROUP = "SlotMarker"
 
 @export var drop_position: Node2D
-@export var stock_light: TextureButton
 @onready var color_rect = $ColorRect
 
 @export var slot_coord: Vector2
-
-@export var out_of_stock_color := Color.RED
-@export var full_stock_color := Color.GREEN
-@export var half_stock_color := Color.ORANGE
 
 var current_product: ProductResource
 var latest_node
 
 func _ready():
 	add_to_group(GROUP)
-	
-	stock_light.pressed.connect(func(): GameManager.fill_slot(slot_coord))
 	
 	color_rect.mouse_entered.connect(func(): color_rect.z_index = 10)
 	color_rect.mouse_exited.connect(func(): color_rect.z_index = 0)
@@ -35,18 +28,14 @@ func _ready():
 			
 			_remove_all()
 			latest_node = _add_scene()
-			_update_stock_color()
 	)
 	GameManager.slot_filled.connect(func(slot, amount):
 		if slot == slot_coord:
-			_update_stock_color()
 			if latest_node == null:
 				latest_node = _add_scene()
 	)
 	GameManager.slot_sold.connect(func(slot, earned):
 		if slot == slot_coord:
-			_update_stock_color()
-			
 			latest_node.drop(drop_position.global_position.y)
 			if GameManager.get_slot_amount(slot_coord) > 0:
 				latest_node = _add_scene()
@@ -64,7 +53,7 @@ func cancel():
 
 func _remove_all():
 	for c in get_children():
-		if c == color_rect or c == stock_light: continue
+		if c == color_rect: continue
 		remove_child(c)
 
 func _add_scene():
@@ -72,12 +61,3 @@ func _add_scene():
 	add_child(node)
 	move_child(node, 0)
 	return node
-
-func _update_stock_color():
-	var p = GameManager.get_slot_amount(slot_coord) / float(GameManager.items_per_slot)
-	var color = half_stock_color
-	if p > 0.5:
-		color = full_stock_color
-	elif p == 0:
-		color = out_of_stock_color
-	stock_light.modulate = color
